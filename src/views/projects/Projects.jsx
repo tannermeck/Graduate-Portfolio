159f707ece86c7c9c12ba35fab91b6ca66a47bfe
TANNER MECK <steve.meck246@gmail.com>
2022-02-01 13:21:52 -0800

    image carousel now rendering

--- src/views/projects/Projects.jsx
+++ src/views/projects/Projects.jsx
@@ -1,9 +1,31 @@
-
+import style from './projects.css';
+import alcodeHome from '../../assets/alcode/alcode-home.png';
+import alcodeSelect from '../../assets/alcode/alcode-select.png';
+import alcodeMap from '../../assets/alcode/alcode-map.png';
+import alcodeCampground from '../../assets/alcode/alcode-campground.png';
+import Carousel from 'nuka-carousel';
 
 export default function Projects(){
-    return (
-        <>
-            <h1>Scroll to see my projects</h1>
-        </>
-    )
+        return (
+            <section className={style.projectsContainer}>
+                <h1>Tanner's Projects</h1>
+                <div className={style.carouselDiv}>
+                <h3 className={style.projectName}>Alcode-Campgrounds</h3>
+                    <Carousel width='100%' autoplay={true} autoplayInterval={5000}>
+                        <div>
+                            <img className={style.carouselImg} src={alcodeHome}/>
+                        </div>
+                        <div>
+                            <img className={style.carouselImg} src={alcodeSelect} />
+                        </div>
+                        <div>
+                            <img className={style.carouselImg} src={alcodeMap} />
+                        </div>
+                        <div>
+                            <img className={style.carouselImg} src={alcodeCampground} />
+                        </div>
+                    </Carousel>
+                </div>
+            </section>
+        )
 }
