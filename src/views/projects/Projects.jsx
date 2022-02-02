d46caf459c1e3b01b20cb1b38d6f72154a3d7224
TANNER MECK <steve.meck246@gmail.com>
2022-02-01 16:21:50 -0800

    spending tracker app rendering under projects

--- src/views/projects/Projects.jsx
+++ src/views/projects/Projects.jsx
@@ -10,7 +10,16 @@ import zoomIn from '../../assets/alcode/zoom-in.png';
 import campDetail from '../../assets/alcode/campdetail.png';
 import campImage from '../../assets/alcode/camp-image.png';
 import favorites from '../../assets/alcode/favorites.png';
-
+import signin from '../../assets/spending/signin.png';
+import home from '../../assets/spending/home.png';
+import menu from '../../assets/spending/menu.png';
+import chart from '../../assets/spending/chart.png';
+import addRecurring from '../../assets/spending/addRecurring.png';
+import category from '../../assets/spending/category.png';
+import newPurchase from '../../assets/spending/newPurchase.png';
+import stopRecurring from '../../assets/spending/stopRecurring.png';
+import deletePurchase from '../../assets/spending/delete.png';
+import subcategory from '../../assets/spending/subcategory.png';
 import Carousel from 'nuka-carousel';
 
 export default function Projects(){
@@ -19,7 +28,7 @@ export default function Projects(){
                 <h1>Tanner's Projects</h1>
                 <div className={style.carouselDiv}>
                     <h3 className={style.projectName}>Alcode-Campgrounds</h3>
-                    <Carousel width='100%' autoplay={true} autoplayInterval={5000} >
+                    <Carousel width='100%' autoplay={true} autoplayInterval={3000} >
                     {/* renderBottomCenterControls={({position: null})} */}
                     {/* need to style bottom controls so they do not cover footer */}
                         <div>
@@ -55,6 +64,21 @@ export default function Projects(){
                         <div>
                             <img className={style.carouselImg} src={alcodeCampground} />
                         </div>
+                    </Carousel>
+                </div>
+                <div className={style.spendingDiv}>
+                    <h3>Spending Tracker</h3>
+                    <Carousel autoplay={true} autoplayInterval={3000}>
+                        <img className={style.spendingImg} src={signin} alt="signin" />
+                        <img className={style.spendingImg} src={home} alt="home" />
+                        <img className={style.spendingImg} src={chart} alt="chart" />
+                        <img className={style.spendingImg} src={subcategory} alt="subcategory" />
+                        <img className={style.spendingImg} src={category} alt="category" />
+                        <img className={style.spendingImg} src={menu} alt="menu" />
+                        <img className={style.spendingImg} src={newPurchase} alt="newPurchase" />
+                        <img className={style.spendingImg} src={addRecurring} alt="addRecurring" />
+                        <img className={style.spendingImg} src={stopRecurring} alt="stopRecurring" />
+                        <img className={style.spendingImg} src={deletePurchase} alt="deletePurchase" />
                     </Carousel>
                 </div>
             </section>
